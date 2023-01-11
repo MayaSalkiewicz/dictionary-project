55569a6a527439044337ed0d5d4b130066dff7dd
Maya <salkiewicz.maya@gmail.com>
2023-01-11 16:27:52 +0100

    adding photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,22 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 
 import Result from "./Result";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyword] = useState("happiness");
   let [result, setResult] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
     setResult(response.data[0]);
   }
 
+  function handlePhotoResponse(response) {
+    console.log(response.data.photos);
+    setPhotos(response.data.photos);
+  }
+
   function Search() {
     setLoaded(true);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let PexelsApiKey =
+      "563492ad6f917000010000011743225eedc94edd8e4b8f6557fc36b9";
+    let apiPexelsUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=9`;
+    let headers = { Authorization: `Bearer ${PexelsApiKey}` };
+    axios.get(apiPexelsUrl, { headers: headers }).then(handlePhotoResponse);
   }
 
   function searchWordMeaning(event) {
@@ -44,6 +57,7 @@ export default function Dictionary() {
           <div className="hint">self-love, happiness, yoga, hobby, wine...</div>
         </section>
         <Result results={result} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
